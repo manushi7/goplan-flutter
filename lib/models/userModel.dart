9b49cd81a3349a1b1876963c5fd5ca822d8c58dc
bisheshbh <bishesh.bhattarai@introcept.co>
2021-12-25 01:03:24 +0545

    Changes made

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -1,2 +1,23 @@
 import 'dart:convert';
 
+import 'package:get/get_connect/http/src/response/response.dart';
+
+User responseFromJson(String str) => User.fromJson(json.decode(str));
+
+String responseToJson(User data) => json.encode(data.toJson());
+
+class User {
+  User({this.email, this.password});
+  String? email;
+  String? password;
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        email: json["email"],
+        password: json["password"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "password": password,
+      };
+}
